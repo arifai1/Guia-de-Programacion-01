e6c639e1fa66ff36daa3f8e4d62ea75677489bec
ari <arifaiwu@gmail.com>
2022-03-28 12:11:31 -0300

    Excercise 6

---
+++ Assets/Ejercicio06.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio06 : MonoBehaviour
+{
+    // Ejercicio 6. Realizá un programa que permita el ingreso por Inspector de un valor entero mayor que 0
+    //en una variable llamada num1 y muestre un mensaje por pantalla indicando "el número es par
+    //" o "el número es impar";. Deberá utilizar el operador “módulo” es el caracter %.
+    public int num1;
+
+    void Start()
+    {
+        if (num1 % 2 == 0)
+        {
+            Debug.Log("El numero es par");
+        }
+        else
+        {
+            Debug.Log("El numero es impar");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
