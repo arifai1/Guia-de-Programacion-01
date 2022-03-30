3ea8b8b905e83dfd479a15558ada24f0aa853655
ari <arifaiwu@gmail.com>
2022-03-30 14:46:37 -0300

    Finish excercise 13
    Se realizo y termino el ejercicio 13

--- Assets/Ejercicio13.cs
+++ Assets/Ejercicio13.cs
@@ -5,9 +5,29 @@ using UnityEngine;
 public class Ejercicio13 : MonoBehaviour
 {
     // Start is called before the first frame update
+    //13. Un obrero necesita calcular su salario semanal, el cual se obtiene de la siguiente  manera:  
+    //• Si trabaja 40 horas o menos se le paga $16 por hora(crear una constante para almacenar el 16)
+    //• Si trabaja más de 40 horas se le paga $16 por cada una de las primeras 40 horas y $20  por cada hora extra. (crear una constante para almacenar el 20)
+    //Ingresar por Inspector la cantidad de horas trabajadas en la semana, y mostrar por pantalla el salario correspondiente
+    float salario;
+    int hora = 16;
+    public int horas;
     void Start()
     {
-        
+        if(horas <= 40)
+        {
+            salario = horas * 16;
+            Debug.Log("Su salario es: " + salario);
+        }
+        else if (horas > 40)
+        {
+            salario = 40 * 16;
+           int extra = horas - 40;
+            extra = extra * 20;
+            salario = salario + extra;
+            Debug.Log("Su salario con las horas extra incluidas es de " + salario);
+        }
+          
     }
 
     // Update is called once per frame
