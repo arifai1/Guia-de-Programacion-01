c43b1900f5e64253fedfe71b999eb6af8dc4593a
ari <arifaiwu@gmail.com>
2022-04-04 11:09:23 -0300

    Update excersices 14 and 15, not finished

--- Assets/Ejercicio14.cs
+++ Assets/Ejercicio14.cs
@@ -5,9 +5,39 @@ using UnityEngine;
 public class Ejercicio14 : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    //     14. Construir un programa que simule el funcionamiento de una calculadora que puede
+    // realizar las cuatro operaciones aritméticas básicas (suma, resta, producto y división) con
+    // valores numéricos enteros.
+    // Se debe especificar la operación que se desea realizar ingresando una letra por Inspector (s
+    // para la suma, r para la resta, p para el producto y d para la división) y luego, deberán
+    // ingresarse dos números enteros para así realizar la operación. Informar el resultado por
+    // pantalla. Nota: Se recomienda el empleo de una sentencia switch.
+
+
+    public int num1;
+    public int num2;
+    public string letra;
+
     void Start()
     {
-        
+        switch (letra)
+        {
+            case 's':
+                Debug.Log("La suma entre ambos numeros es " num1 + num2);
+                break;
+            case "r":
+                int resta = num1 - num2;
+                int resta1 = num2 - num1;
+                Debug.Log("La resta entre " + num1 + "-" + num2 + " es igual a " + resta+ "y la resta entre " + num2 + " y" + num1 + " es igual a " + resta1);
+                break;
+            case 'p':
+                Debug.Log("El producto entre ambos numeros es " + num1 * num2);
+                break;
+            default:
+                Debug.Log("La division entre " + num1 + " y" + num2 + " es igual a " + num1 / num2 + " y la division entre " + num2 + " y" + num1 + " es igual a " + num2 / num1 +);
+                break;
+        }
     }
 
     // Update is called once per frame
--- Assets/Ejercicio15.cs
+++ Assets/Ejercicio15.cs
@@ -5,9 +5,45 @@ using UnityEngine;
 public class Ejercicio15 : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    //   15. Existen dos reglas que identifican dos conjuntos de valores:
+    // - El número es de un solo dígito.
+    // - El número es impar.
+    // A partir de estas reglas, creá un algoritmo que asigne a las variables booleanas estaEnA,
+    // estaEnB, estaEnAmbos y noEstaEnNinguno el valor verdadero o falso, según corresponda,
+    // para indicar si el valor ingresado pertenece al primer conjunto, al segundo, a ambos o a
+    // ninguno, respectivamente. Definí un lote de prueba de varios números y probá el algoritmo,
+    // escribiendo los resultados.
+
+    public float num;
+    public float num1;
+    bool estaEnA = false;
+    bool estaEnB = false;
+    bool estaEnAmbos = false;
+    bool noestaEnNinguno = false;
+
     void Start()
     {
-        
+        if (num < 10 && num1 > 0)
+        {
+            estaEnA = true;
+            Debug.Log("El valor es de un solo digito");
+        }
+        else if (num % 2 > 0)
+        {
+            estaEnB = true;
+            Debug.Log("El valor es de un solo digito");
+        }
+        else if (num % 2 > 0 || num < 10 num1 > 0)
+        {
+            estaEnAmbos = true;
+            Debug.Log("El numero es impar y es de un solo digito");
+        }
+        else
+        {
+            noestaEnNinguno = true;
+            Debug.Log("El numero no esta en ninguno");
+        }
     }
 
     // Update is called once per frame
